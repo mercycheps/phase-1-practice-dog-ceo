ad76934203a148bcb8b82497b16710455f49f3c7
Mercy <maritimmercy8@gmail.com>
2025-03-30 15:28:35 +0300

    added fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        const imageContainer = document.getElementById("dog-image-container");
+        data.message.forEach(imgUrl => {
+            const img = document.createElement("img");
+            img.src = imgUrl;
+            img.alt = "A dog";
+            img.style.width = "250px";
+            imageContainer.appendChild(img);
+        
+        })
+    });
+});
+
