8c926de7c7a5e4df0014c289c5a97f1f0796c067
Mercy <maritimmercy8@gmail.com>
2025-04-01 20:43:16 +0300

    added a dropdown & filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('%c HI', 'color: firebrick')
-
+// fetching images of dogs
 document.addEventListener("DOMContentLoaded", () => {
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
     
@@ -16,22 +16,54 @@ document.addEventListener("DOMContentLoaded", () => {
         
         })
     });
-});
+    // fetching breed of dogs
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
-document.addEventListener("DOMContentLoaded",() => {
-const breedURL = "https://dog.ceo/api/breeds/list/all";
-})
 fetch(breedUrl)
 .then (response => response.json())
 .then(data => {
     const imageContainer = document.getElementById("dog-image-container");
-    data.message.forEach(breedUrl => {
-        const img = document.createElement("img")
-        img.src = breedUrl;
-        img.alt = "A dog";
-          
+   console.log(data)
+   for (let i in data.message){
+    console.log(i);
+   }
+})
+});
+// Funtion of breed list
+ function renderBreeds(breeds) {
+    breedList.innerHTML = "";
+    breeds.forEach(breed => {
+        const li = document.createElement("li");
+            li.textContent = breed;
+            li.style.cursor = "pointer";
+            li.addEventListener("click", () => {
+                li.style.color = "blue";
+            });
+            breedList.appendChild(li);
+
+    });
+ }
+ // Event listener to filter breed
+ breedDropdown.addEventListener("change", (event) => {
+    const selectedLetter = event.target.value;
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => {
+        const filteredBreeds = Object.keys(data.message).filter(breed => breed.startsWith(selectedLetter));
+        renderBreeds(filteredBreeds);
     })
 
-})      
+ })
+
+
+ 
+
+
+
+
+
+
+
+    
 
 
